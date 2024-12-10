8c629e993a7c05d52ec06f69a45ea4b450afb0ea
Mihail Sergeevich <seetch@inbox.ru>
2024-12-10 03:27:00 +0300

    Add log type to queue

--- src/main/java/me/seetch/telegramIntegration/TelegramIntegration.java
+++ src/main/java/me/seetch/telegramIntegration/TelegramIntegration.java
@@ -2,8 +2,6 @@ package me.seetch.telegramIntegration;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,28 +10,20 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.Queue;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final class TelegramIntegration extends JavaPlugin {
 
     private TelegramBot bot;
-    private final Map<String, Map<Integer, Queue<String>>> messageQueues = new ConcurrentHashMap<>();
-    private ScheduledExecutorService scheduler;
+    private final Map<String, Map<String, Map<Integer, Queue<String>>>> messageQueues = new ConcurrentHashMap<>();
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
         bot = new TelegramBot.Builder(getConfig().getString("token")).build();
-        bot.setUpdatesListener(updates -> {
-            for (Update update : updates) {
-                handleUpdate(update);
-            }
-            return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        });
-
-        scheduler = Executors.newScheduledThreadPool(1);
-        startScheduler();
+        bot.setUpdatesListener(updates -> UpdatesListener.CONFIRMED_UPDATES_ALL);
     }
 
     @Override
@@ -41,71 +31,45 @@ public final class TelegramIntegration extends JavaPlugin {
         if (bot != null) {
             bot.removeGetUpdatesListener();
         }
-
-        stopScheduler();
     }
 
-    public void addMessage(String chatId, Integer messageThreadId, String message) {
+    public void addMessage(String logType, String chatId, Integer messageThreadId, String message) {
         String timestamp = LocalDateTime.now(ZoneId.of("Europe/Moscow"))
                 .format(DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy"));
 
         String formattedMessage = "[" + timestamp + "] " + message.replaceAll("([&§])[0-9a-fk-or]", "");
 
-        if (messageThreadId == null) {
-            messageQueues.computeIfAbsent(chatId, k -> new ConcurrentHashMap<>())
-                    .computeIfAbsent(0, k -> new ConcurrentLinkedQueue<>())
-                    .add(formattedMessage);
-        } else {
-            messageQueues.computeIfAbsent(chatId, k -> new ConcurrentHashMap<>())
-                    .computeIfAbsent(messageThreadId, k -> new ConcurrentLinkedQueue<>())
-                    .add(formattedMessage);
+        Queue<String> queue = messageQueues
+                .computeIfAbsent(logType, k -> new ConcurrentHashMap<>())
+                .computeIfAbsent(chatId, k -> new ConcurrentHashMap<>())
+                .computeIfAbsent(messageThreadId != null ? messageThreadId : 0, k -> new ConcurrentLinkedQueue<>());
+
+        queue.add(formattedMessage);
+
+        if (queue.size() >= 10) {
+            sendMessages(logType, chatId, messageThreadId != null ? messageThreadId : 0, queue);
         }
     }
 
+    private void sendMessages(String logType, String chatId, int messageThreadId, Queue<String> queue) {
+        StringBuilder messageBuilder = new StringBuilder();
+        int count = 0;
 
-    private void startScheduler() {
-        scheduler.scheduleAtFixedRate(() -> {
-            for (Map.Entry<String, Map<Integer, Queue<String>>> chatEntry : messageQueues.entrySet()) {
-                String chatId = chatEntry.getKey();
-                for (Map.Entry<Integer, Queue<String>> threadEntry : chatEntry.getValue().entrySet()) {
-                    int messageThreadId = threadEntry.getKey();
-                    Queue<String> queue = threadEntry.getValue();
-                    StringBuilder messageBuilder = new StringBuilder();
-                    while (!queue.isEmpty()) {
-                        messageBuilder.append(queue.poll()).append("\n");
-                    }
-                    if (!messageBuilder.isEmpty()) {
-                        sendMessage(chatId, messageThreadId, messageBuilder.toString());
-                    }
-                }
-            }
-        }, 0, 30, TimeUnit.SECONDS);
+        while (!queue.isEmpty() && count < 10) {
+            messageBuilder.append(queue.poll()).append("\n");
+            count++;
+        }
+
+        if (!messageBuilder.isEmpty()) {
+            sendMessage(logType, chatId, messageThreadId, messageBuilder.toString());
+        }
     }
 
-    private void stopScheduler() {
-        scheduler.shutdown();
-    }
-
-    private void sendMessage(String chatId, int messageThreadId, String message) {
-        SendMessage request = new SendMessage(chatId, message);
+    private void sendMessage(String logType, String chatId, int messageThreadId, String message) {
+        SendMessage request = new SendMessage(chatId, "#" + logType.toUpperCase() + "\n\n" + message);
         if (messageThreadId != 0) {
             request.messageThreadId(messageThreadId);
         }
         bot.execute(request);
-    }
-
-    private void handleUpdate(Update update) {
-        if (update.message() != null) {
-            Message message = update.message();
-            if (message.text() != null && message.text().startsWith("/integration")) {
-                String chatId = message.chat().id().toString();
-                Integer messageThreadId = message.messageThreadId();
-                String response = "💬 Chat ID: " + chatId;
-                if (messageThreadId != null) {
-                    response += "\n📦 Topic ID: " + messageThreadId;
-                }
-                sendMessage(chatId, messageThreadId != null ? messageThreadId : 0, response);
-            }
-        }
     }
 }
